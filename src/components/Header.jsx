3ba7595aa7f506cf0e800a19d6075aac9f607dd4
Your Name <youremail@example.com>
2025-03-31 22:16:20 +0500

    hello

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Olimjon</h1>
+      <Header name="John Doe" />
+    </div>
+  );
+};
+
+export default Dashboard;
